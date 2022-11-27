c8385e4d03af86057b2108a98a8aa2699b7a000d
Takaya Yamazoe <ytakaya@TakayanoMacBook-Air.local>
2022-11-27 23:51:40 +0900

    add install scripts

---
+++ scripts/setup.bash
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -eux
+# shellcheck source=./scripts/common.bash
+source "$(dirname "$0")/common.bash"
+
+/bin/bash "$CUR_DIR/setup-homebrew.bash"
+# /bin/bash "$CUR_DIR/setup-links.bash"
+# /bin/bash "$CUR_DIR/setup-mac.bash"
+# /bin/bash "$CUR_DIR/setup-zinit.bash"
+# /bin/bash "$CUR_DIR/setup-dein.bash"
