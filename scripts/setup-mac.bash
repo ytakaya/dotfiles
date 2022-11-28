136db0b47203765f7255ecf888e871824eb5950d
ytakaya <mt.tky@icloud.com>
2022-11-28 19:23:05 +0900

    for vscode

--- scripts/setup-mac.bash
+++ scripts/setup-mac.bash
@@ -10,3 +10,13 @@ defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Configure hammerspoon config location
 defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
+
+# VSCode
+VSCODE_SETTING_DIR="$HOME/Library/Application\ Support/Code/User"
+rm "$VSCODE_SETTING_DIR/settings.json"
+cp "$REPO_DIR/config/vscode/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
+# install extention
+cat "$REPO_DIR/config/vscode/extensions" | while read line
+do
+    code --install-extension $line
+done
