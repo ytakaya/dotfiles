db3f0f8cdc229febc087a1832625c6f12e978099
Yamazoe Takaya <yamazoe.takaya@enechain.co.jp>
2024-12-06 21:31:01 +0900

    polish

--- scripts/setup-links.bash
+++ scripts/setup-links.bash
@@ -11,8 +11,7 @@ fi
 mkdir -p \
     "$XDG_CONFIG_HOME" \
     "$XDG_STATE_HOME" \
-    "$XDG_DATA_HOME/vim" \
-    "$XDG_CONFIG_HOME/tmux"
+    "$XDG_DATA_HOME/vim"
 
 ln -sfv "$REPO_DIR/config/"* "$XDG_CONFIG_HOME"
 ln -sfv "$XDG_CONFIG_HOME/zsh/.zshenv" "$HOME/.zshenv"
